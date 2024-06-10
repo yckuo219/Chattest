e78020d6f969e34b723153c44ea0aa7c2a4eb20f
yckuo219 <1110058@kids.jgjhs.tyc.edu.tw>
2024-06-10 20:21:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-const port = 3000;
-
-// 設置靜態文件夾
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 設置主路由
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
 });
 
-// 啟動伺服器
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
